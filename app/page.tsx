6fe9baec37a3032540d79abade239fbb8fba3bdb
本間敬太 <keitakunv2@gmail.com>
2024-10-31 23:29:35 +0900

    🎨

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <main>
       <div className="container mx-auto px-4 py-8">
-        <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           <LinkCards imgName="crombas" title="クロムバス攻略" />
           <LinkCards imgName="trade" title="物々交換リスト" />
         </div>
