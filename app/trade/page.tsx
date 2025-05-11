7b6b20f8a9cd94989b708e26b9a17d9f495bb9db
本間敬太 <keitakunv2@gmail.com>
2025-05-11 13:56:50 +0900

    ⚡️ 10時間に変更

--- app/trade/page.tsx
+++ app/trade/page.tsx
@@ -1,11 +1,17 @@
-import { Tradetable } from '@/components/tradetable/table';
+import { Tradetable } from '@/components/tradetable/Tradetable';
 import { fetchNotionData } from '@/lib/notion';
 import { TradeObj } from '../types/types';
 
-export const revalidate = 0;
+//10時間ごとに再生成
+export const revalidate = 36000;
 
 const Home = async () => {
   const databaseId = process.env.NOTION_DATABASE_ID || '';
+  if (!databaseId) {
+    throw new Error('NOTION_DATABASE_ID is not defined');
+  }
+
+  // NotionのデータベースIDを環境変数から取得
   const trades = (await fetchNotionData(databaseId)) as unknown as TradeObj[];
   return (
     <main className="mb-28">
